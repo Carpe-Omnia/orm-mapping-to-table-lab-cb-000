c035edd6a3c5723b8fa3abed1727de60d407f9ab
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-31 19:23:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,5 +9,10 @@ class Student
     @id = id
   end
   def self.create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
 end
