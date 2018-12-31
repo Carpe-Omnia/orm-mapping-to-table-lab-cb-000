de86464accbabc940510182551d6f0fbead67f22
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-31 19:28:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,10 +14,14 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-      )
+      );
       SQL
     DB[:conn].execute(sql)
   end
   def self.drop_table
-
+    sql = <<-SQL
+      DROP TABLE students :
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
